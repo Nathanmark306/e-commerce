646d787673206afb633b29effe14a7da79dd1262
nathan <nathanmark306@gmail.com>
2022-09-27 10:07:14 +0100

    connecting to mongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,21 @@
 const http = require('http');
-
 const { app } = require('./app');
-
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Listening on PORT: ${PORT}`);
-});
+require('dotenv').config();
+
+mongoose.connection.once('open', () => console.log('DB connection ready'));
+mongoose.connection.on('error', (error) => console.error(error));
+
+async function loadOnStart() {
+  await mongoose.connect(process.env.MONGODB_URL);
+  //'mongodb+srv://ecommerce:7EpEUDMoHkvwEfWp@nasacluster.znbvauu.mongodb.net/?retryWrites=true&w=majority'
+
+  server.listen(PORT, () => {
+    console.log(`Listening on PORT: ${PORT}`);
+  });
+}
+
+loadOnStart();
